6494319d40b8c4eeefcf7d493fee9223e65812ea
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-14 16:17:37 +0530

    XAMARIN-34267 - Sample and screen shot added

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/ItemTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.TreeView.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class ItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate RootNodeTemplate { get; set; }
+        public DataTemplate ChildNodeTemplate { get; set; }
+
+        public DataTemplate CollapsedTemplate { get; set; }
+
+        public ItemTemplateSelector()
+        {
+            this.RootNodeTemplate = new DataTemplate(typeof(RootNodeTemplate));
+            this.ChildNodeTemplate = new DataTemplate(typeof(ChildNodeTemplate));
+            this.CollapsedTemplate = new DataTemplate(typeof(CollapsedTemplate));
+        }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var treeviewNode = item as TreeViewNode;
+            if (treeviewNode == null)
+                return null;
+            if (!treeviewNode.IsExpanded && treeviewNode.HasChildNodes && treeviewNode.Level == 0)
+                return this.CollapsedTemplate;
+            else if (treeviewNode.Level == 0)
+                return this.RootNodeTemplate;
+            else
+                return this.ChildNodeTemplate;
+        }
+    }
+}
